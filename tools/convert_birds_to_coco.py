0c171c394037a4182a889081cadf47915ccd746d
Mohammad Farahmand <mohammedfarahmand98@gmail.com>
2022-10-19 14:18:53 +0330

    Updated the "conversion" solution. The dataset that can be downloaded on Colab has a slightly different structure.

--- tools/convert_birds_to_coco.py
+++ tools/convert_birds_to_coco.py
@@ -7,30 +7,29 @@ import scipy.io as io
 from PIL import Image
 
 
+def read_list(filepath, flatten, type=None):
+    with open(data_dir / filepath) as stream:
+        items = [line.rstrip().split() for line in stream.readlines()]
+    li = [rest for index, *rest in items]
+    if type is not None:
+        li = [[type(el) for el in item] for item in li]
+    if flatten:
+        li = [el for item in li for el in item]
+    return li
+
+
 def get_image_info(filepath):
     image = Image.open(data_dir / 'images' / filepath)
     width, height = image.size
     return {'file_name': os.path.join('images', filepath), 'width': width, 'height': height, 'frame_id': 1}
 
 
-def get_image_annotation(filepath, image_id, individual):
-    if individual:
-        category_id, *rest = filepath.split('.')
-    else:
-        category_id = 1
-    filepath = data_dir / 'annotations-mat' / filepath
-    filepath, extension = os.path.splitext(filepath)
-    annotation = io.loadmat(f'{filepath}.mat')
-    box = annotation['bbox']  # Left, Top, Right, Bottom
-    left, top, right, bottom = [box[0, 0][i].item() for i in range(4)]
-    box = [left, top, right - left, bottom - top]
+def create_annotation(box):
+    left, top, width, height = box
     return {
-        'category_id': int(category_id),
-        'image_id': image_id,
         'track_id': -1,
-        'bbox_vis': box,
-        'bbox': box,
-        'area': (right - left) * (bottom - top),
+        'bbox_vis': box, 'bbox': box,
+        'area': width * height,
         'iscrowd': 0  # Have no clue what this is!
     }
 
@@ -39,39 +38,34 @@ if __name__ == '__main__':
     # Command Line Arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', default='datasets/CaltechBirds')
-    parser.add_argument('--individual', action='store_true')
-    parser.add_argument('--merged', action='store_true')
-    parser.add_argument('--skip-annotations', action='store_true')
+    parser.add_argument('--separate', action='store_true')
     # Directories Setup
     args = parser.parse_args()
     data_dir = pathlib.Path(args.dir)
     save_to = data_dir / 'annotations'
     if not save_to.exists():
         os.mkdir(save_to)
-    # Initializations
-    classes = data_dir / 'lists' / 'classes.txt'
-    with open(classes) as stream:
-        classes = [line.rstrip().split() for line in stream.readlines()]
-    if args.individual:
-        categories = [{'id': index, 'name': name} for index, name in classes]
+    # Lists and Labels
+    classes = read_list('classes.txt', flatten=True)
+    filepaths = read_list('images.txt', flatten=True)
+    boxes = read_list('bounding_boxes.txt', flatten=False, type=float)
+    splits = read_list('train_test_split.txt', flatten=True, type=int)
+    labels = read_list('image_class_labels.txt', flatten=True, type=int)
+    assert len(filepaths) == len(boxes) == len(splits) == len(labels)
+    # Classes for Export
+    if not args.separate:
+        categories = [{'id': index + 1, 'name': name} for index, name in enumerate(classes)]
     else:
         categories = [{'id': 1, 'name': 'Any_Bird'}]
-    # Exporting
-    subsets = ['files'] if args.merged else ['train', 'test']
-    for subset in subsets:
-        output = {'images': None, 'annotations': None, 'categories': categories}
-        print(f'Processing {subset}.txt ...')
-        # Image Files
-        with open(data_dir / 'lists' / f'{subset}.txt') as stream:
-            filepaths = [line.rstrip() for line in stream.readlines()]
-        images = map(get_image_info, filepaths)
-        output['images'] = [{'id': index, 'video_id': index, **di} for index, di in enumerate(images)]
-        # Annotations
-        annotations = []
-        if not args.skip_annotations:
-            for index, filepath in enumerate(filepaths):
-                annotation = get_image_annotation(filepath, index, args.individual)
-                annotations.append({'id': len(annotations), **annotation})
-        output['annotations'] = annotations
-        # Saving
-        json.dump(output, open(save_to / f'{subset if not args.merged else "train"}.json', 'w'))
+    # Images and Annotations
+    subsets = ('train', 'test')
+    images = [[] for subset in subsets]
+    annotations = [[] for subset in subsets]
+    for filepath, box, split, label in zip(filepaths, boxes, splits, labels):
+        index = len(images[split]) + 1
+        category_id = label if args.separate else 1
+        images[split].append({'id': index, **get_image_info(filepath)})
+        annotations[split].append({'id': index, 'image_id': index, 'category_id': category_id, **create_annotation(box)})
+    for split, name in enumerate(subsets):
+        output = {'images': images[split], 'annotations': annotations[split], 'categories': categories}
+        json.dump(output, open(save_to / f'{name}.json', 'w'))
